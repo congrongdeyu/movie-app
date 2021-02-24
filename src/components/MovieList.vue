4c90cf2dc4f86ce0c5263e5ed6b2bccaf9564bf3
congrongdeyu <congrongdeyu@gmail.com>
2021-02-24 22:38:31 +0800

    create Sidebar and MovieList

---
+++ src/components/MovieList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="movie-container">
+    Movie list
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieList"
+}
+</script>
+
+<style lang='scss' scoped>
+#movie-container {
+  padding-top: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  flex-grow: 2;
+  justify-content: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="home">
     <navbar/>
+
+    <div id="main-section">
+      <sidebar/>
+      <movie-list/>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
+import MovieList from "../components/MovieList"
 
 export default {
   name: 'Home',
   components: {
-    Navbar
+    Navbar,
+    Sidebar,
+    MovieList
   }
 }
 </script>
